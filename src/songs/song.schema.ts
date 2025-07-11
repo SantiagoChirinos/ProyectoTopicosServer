0b62a70c212ec6f27620ef7467cf9e5e7b2aa37a
SantiagoChirinos <sachirinos.23@est.ucab.edu.ve>
2025-07-10 23:37:08 -0400

    -docs: documentacion de songs
    añadida toda la documentacion js doc de lo relacionado a songs

--- src/songs/song.schema.ts
+++ src/songs/song.schema.ts
@@ -1,5 +1,15 @@
 import { Schema } from 'mongoose';
 
+/**
+ * Esquema de la cancion para la base de datos
+ * 
+ * @interface Song
+ * @property {string} nombre - Título de la canción.
+ * @property {string} artista - Nombre del artista o banda que interpreta la canción.
+ * @property {string} genero - Género musical de la canción (ej. rock, pop, reggaetón).
+ * @property {number} anio - Año de lanzamiento de la canción.
+ * @property {number} reproducciones - Número total de veces que se ha reproducido.
+ */
 export const SongSchema = new Schema({
   nombre: { type: String, required: true },
   artista: { type: String, required: true },
@@ -8,5 +18,7 @@ export const SongSchema = new Schema({
   reproducciones: { type: Number, required: true },
 });
 
-// Índice único para evitar duplicados por nombre y artista
+/**
+ * Índice único para evitar duplicados por nombre y artista
+ */
 SongSchema.index({ nombre: 1, artista: 1 }, { unique: true });
